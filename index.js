74b35b9a46ac20db4b0556fb9b11186a0344e021
Yin-Ling Wei <cuckoo123321@gmail.com>
2024-10-02 15:00:47 +0800

    remove some baseURL

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function redirectBack(req, res){
 }
 
 //設置路由處理
-app.get(`${baseUrl}/`,(req, res) => {
+app.get('/',(req, res) => {
   const admin_full_name = req.session.admin_full_name; // 從 session 中獲取變數
   res.render('index',{ admin_full_name });// 傳遞變數到模板
 });
@@ -70,25 +70,25 @@ app.get(`${baseUrl}/`,(req, res) => {
 
 
 //admin
-app.get(`${baseUrl}/login`, adminController.login);
-app.post(`${baseUrl}/login`, adminController.handleLogin, redirectBack);
-app.get(`${baseUrl}/logout`, adminController.logout);
-app.get(`${baseUrl}/adminAdd`, adminController.add);
-app.post(`${baseUrl}/adminAdd`, adminController.handleAdd, redirectBack);
-app.get(`${baseUrl}/adminList`, adminController.getAll);
-app.get(`${baseUrl}/delete_admin/:id`, adminController.delete);
-app.get(`${baseUrl}/update_admin/:id`, adminController.update);
-app.post(`${baseUrl}/update_admin/:id`, adminController.handleUpdate);
-app.get(`${baseUrl}/search`, adminController.search);
+app.get(`/login`, adminController.login);
+app.post(`/login`, adminController.handleLogin, redirectBack);
+app.get(`/logout`, adminController.logout);
+app.get(`/adminAdd`, adminController.add);
+app.post(`/adminAdd`, adminController.handleAdd, redirectBack);
+app.get(`/adminList`, adminController.getAll);
+app.get(`/delete_admin/:id`, adminController.delete);
+app.get(`/update_admin/:id`, adminController.update);
+app.post(`/update_admin/:id`, adminController.handleUpdate);
+app.get(`/search`, adminController.search);
 
 //user
-app.get(`${baseUrl}/userAdd`, userController.add);
-app.post(`${baseUrl}/userAdd`, userController.handleAdd, redirectBack);
-app.get(`${baseUrl}/userList`, userController.getAll);
-app.get(`${baseUrl}/update_user/:id`, userController.update);
-app.post(`${baseUrl}/update_user/:id`, userController.handleUpdate);
-app.get(`${baseUrl}/delete_user/:id`, userController.softDelete);
-app.get(`${baseUrl}/userSearch`, userController.search);
+app.get(`/userAdd`, userController.add);
+app.post(`/userAdd`, userController.handleAdd, redirectBack);
+app.get(`/userList`, userController.getAll);
+app.get(`/update_user/:id`, userController.update);
+app.post(`/update_user/:id`, userController.handleUpdate);
+app.get(`/delete_user/:id`, userController.softDelete);
+app.get(`/userSearch`, userController.search);
 //user前端用 API
 app.post('/userLogin', userController.userLogin);
 app.get('/user', userController.getUserInfo);
@@ -96,37 +96,37 @@ app.post('/userRegister', userController.userRegister);
 app.put('/userUpdate/:id', userController.FrontendHandleUpdate);
 
 //article
-app.get(`${baseUrl}/articleAdd`, articleController.add);
-app.post(`${baseUrl}/articleAdd`, articleController.handleAdd, redirectBack);
-app.get(`${baseUrl}/articleList`, articleController.getAll);
-app.get(`${baseUrl}/update_article/:id`, articleController.update);
-app.post(`${baseUrl}/update_article/:id`, articleController.handleUpdate);
-app.get(`${baseUrl}/delete_article/:id`, articleController.delete);
-app.get(`${baseUrl}/articleSearch`, articleController.search);
+app.get(`/articleAdd`, articleController.add);
+app.post(`/articleAdd`, articleController.handleAdd, redirectBack);
+app.get(`/articleList`, articleController.getAll);
+app.get(`/update_article/:id`, articleController.update);
+app.post(`/update_article/:id`, articleController.handleUpdate);
+app.get(`/delete_article/:id`, articleController.delete);
+app.get(`/articleSearch`, articleController.search);
 //article 前端用API
 app.get('/article', articleController.getArticleData);
 
 //carousel
-app.get(`${baseUrl}/carouselAdd`, carouselController.add);
-app.post(`${baseUrl}/carouselAdd`, carouselController.handleAdd, redirectBack);
-app.get(`${baseUrl}/carouselList`, carouselController.getAll);
-app.get(`${baseUrl}/update_carousel/:id`, carouselController.update);
-app.post(`${baseUrl}/update_carousel/:id`, carouselController.handleUpdate);
-app.get(`${baseUrl}/delete_carousel/:id`, carouselController.delete);
-app.get(`${baseUrl}/carouselSearch`, carouselController.search);
+app.get(`/carouselAdd`, carouselController.add);
+app.post(`/carouselAdd`, carouselController.handleAdd, redirectBack);
+app.get(`/carouselList`, carouselController.getAll);
+app.get(`/update_carousel/:id`, carouselController.update);
+app.post(`/update_carousel/:id`, carouselController.handleUpdate);
+app.get(`/delete_carousel/:id`, carouselController.delete);
+app.get(`/carouselSearch`, carouselController.search);
 //carousel前端API
 app.get('/carousel', carouselController.getCarouselData);
 
 
 
 //product
-app.get(`${baseUrl}/productAdd`, productController.add);
-app.post(`${baseUrl}/productAdd`, productController.handleAdd, redirectBack);
-app.get(`${baseUrl}/productList`, productController.getAll);
-app.get(`${baseUrl}/update_product/:id`, productController.update);
-app.post(`${baseUrl}/update_product/:id`, productController.handleUpdate);
-app.get(`${baseUrl}/delete_product/:id`, productController.delete);
-app.get(`${baseUrl}/productSearch`, productController.search);
+app.get(`/productAdd`, productController.add);
+app.post(`/productAdd`, productController.handleAdd, redirectBack);
+app.get(`/productList`, productController.getAll);
+app.get(`/update_product/:id`, productController.update);
+app.post(`/update_product/:id`, productController.handleUpdate);
+app.get(`/delete_product/:id`, productController.delete);
+app.get(`/productSearch`, productController.search);
 //product前端用API
 app.get('/:id', productController.getProductById);
 app.put('/update_productStock', productController.updateProductStock)
