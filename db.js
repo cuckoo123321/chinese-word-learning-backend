21373151683fe73fc16c2dfcdcb17e83f390a6aa
ccAdmin <cuckoo123321@gmail.com>
2025-10-20 13:31:10 +0800

    註解 node.js 版本 16

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-let mysql = require('mysql');
+let mysql = require('mysql2');
 const { DATABASE_HOST, DATABASE_PORT, DATABASE_USER,  DATABASE_PASSWORD, DATABASE } = process.env;
 
 let connection = mysql.createConnection({    
@@ -10,6 +10,25 @@ let connection = mysql.createConnection({
     database : DATABASE,
     charset: 'utf8mb4'
 });
+
+setInterval(() => {
+    connection.query('SELECT 1', (err) => {
+        if (err) {
+            console.error('MySQL keep-alive error:', err);
+            connection.destroy();
+            connection = mysql.createConnection({ 
+                host: DATABASE_HOST,
+                port: DATABASE_PORT,
+                user: DATABASE_USER,
+                password: DATABASE_PASSWORD,
+                database: DATABASE,
+                charset: 'utf8mb4'
+            });
+        } else {
+            console.log('MySQL keep-alive ping sent');
+        }
+    });
+}, 5 * 60 * 1000);
 
 
 module.exports = connection;
